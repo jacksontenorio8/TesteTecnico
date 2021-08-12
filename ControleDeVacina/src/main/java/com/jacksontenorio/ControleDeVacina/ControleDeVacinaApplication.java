02883d78a93e097537666dae6741765640de4025
Jackson <jacksontenorio8@gmail.com>
2021-08-12 16:21:48 -0300

    Programa Controle de vacinas

---
+++ ControleDeVacina/src/main/java/com/jacksontenorio/ControleDeVacina/ControleDeVacinaApplication.java
@@ -0,0 +1,13 @@
+package com.jacksontenorio.ControleDeVacina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeVacinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeVacinaApplication.class, args);
+	}
+
+}
---
+++ ControleDeVacina/src/test/java/com/jacksontenorio/ControleDeVacina/ControleDeVacinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacksontenorio.ControleDeVacina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeVacinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
